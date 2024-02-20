561be6c7c36d3ae391acc4a4d13b75b1be2577f5
coronel98 <osvaldo.coronel98@hotmail.com>
2024-02-20 17:36:18 -0600

    API call is working

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,12 +1,21 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import BaseTitle from '@/components/BaseTitle.vue';
+import useAPI from '@/composables/useAPI'
+
+const { categories, getCategories } = useAPI()
+
+onMounted(async () => {
+  await getCategories()
+})
+
+
 </script>
 
 <template>
-  <BaseTitle>TRIVIA APPLICATION</BaseTitle>
-  <div>
-  
+  <h1 class="text-center" >TRIVIA APPLICATION</h1>
+  <div class="grid flex-grow grid-cols-4 gap-12 m-20">
+      {{  category.name }}
   </div>  
 
  
